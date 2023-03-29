7115c42a2f929a973d67b73f23772822cf1fab71
Iskanderrus <a.n.chasovskoy@gmail.com>
2023-03-30 00:04:41 +0300

    final version of the spider crawling top 250 movies as per 30th of March 2023

--- imdb/spiders/best_movies.py
+++ imdb/spiders/best_movies.py
@@ -10,7 +10,7 @@ class BestMoviesSpider(CrawlSpider):
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
     def start_requests(self):
-        yield scrapy.Request(url="https://www.imdb.com/chart/top/", headers={"User_Agent":self.user_agent})
+        yield scrapy.Request(url="https://www.imdb.com/chart/top/", headers={"User_Agent": self.user_agent})
 
     rules = (
         Rule(
@@ -26,4 +26,27 @@ class BestMoviesSpider(CrawlSpider):
         return request
 
     def parse_item(self, response):
-        print(response.url)
+        yield {
+            "title": response.xpath('//h1/span/text()').get(),
+            "year": response.xpath('(//section/div/div/ul/li[@role="presentation"])[1]/a/text()').get(),
+            "age": response.xpath('(//section/div/div/ul/li[@role="presentation"])[2]/a/text()').get(),
+            "duration": ' '.join(response.xpath(
+                '((//div[@data-testid="title-techspecs-section"]/ul/li[1]/*)[2]/text())[1] | ((//div['
+                '@data-testid="title-techspecs-section"]/ul/li[1]/*)[2]/text())[3] | ((//div['
+                '@data-testid="title-techspecs-section"]/ul/li[1]/*)[2]/text())[5] | ((//div['
+                '@data-testid="title-techspecs-section"]/ul/li[1]/*)[2]/text())[7]').getall()),
+            "genre": response.xpath('//a/span[@class="ipc-chip__text"]/text()').get(),
+            "rating": response.xpath(
+                '//div[@class="sc-7b68ec71-0 fuKYWn sc-93323f1b-12 bmgiwi"]/div/div/a/span/div/div[2]/div/span['
+                '1]/text()').get(),
+            "user_reviews": response.xpath('//ul/li[1]/a/span/span[@class="score"]/text()').get(),
+            "critic_reviews": response.xpath('//ul/li[2]/a/span/span[@class="score"]/text()').get(),
+            "metascore": response.xpath('//span[@class="score-meta"]/text()').get(),
+            "oscar_nominations": response.xpath('//div[@data-testid="awards"]/ul/li/a/text()').get(),
+            "wins_and_nominations": response.xpath('//div[@data-testid="awards"]/ul/li/div/ul/li/span/text()').get(),
+            "director": response.xpath('//section[@data-testid="title-cast"]/ul/li[1]//a/text()').get(),
+            "country_of_origin": response.xpath('//div[@data-testid="title-details-section"]/ul/li[2]//a/text()').get(),
+            "production_company": response.xpath(
+                '//div[@data-testid="title-details-section"]/ul/li[7]//div//a/text()').get(),
+            "movie_url": response.url,
+        }
